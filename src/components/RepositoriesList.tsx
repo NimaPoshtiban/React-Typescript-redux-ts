3609a10308ce0b8ee79a29f0fcfa9ae7069bb4fb
Nima Poshtiban <poshtibannima@gmail.com>
2021-03-23 13:57:44 +0430

    app finished

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -25,6 +25,9 @@ const RepositoriesList: React.FC = () => {
         />
         <button className="button ui ">Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name)=><div key={name}>{name}</div>)}
     </Fragment>
   );
 };
